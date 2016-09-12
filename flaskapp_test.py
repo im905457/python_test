15f224c3333f9dbafa6beabadf79f691beb07808
im905457 <c6175@hotmail.com>
2016-09-12 12:06:03 +0800

    debug jsonify func

--- flaskapp_test.py
+++ flaskapp_test.py
@@ -8,18 +8,7 @@ app.config['MYSQL_DATABASE_PASSWORD'] = '19860601'
 app.config['MYSQL_DATABASE_DB'] = 'innodb'
 app.config['MYSQL_DATABASE_HOST'] = 'aws-rds.cm1lnnlrbky4.ap-northeast-1.rds.amazonaws.com'
 mysql.init_app(app)
-genres = [
-    {
-        'id': 1,
-        'name': u'Trash Metal',
-        'bands': u'Metallica, Megadeth'
-    },
-    {
-        'id': 2,
-        'name': u'Death Metal',
-        'bands': u'Dark Tranquility, Inflames'
-    }
-]
+
 @app.route('/')
 def hello_world():
 	return 'Hello from EC2 + Ubuntu Flask + git! ver 3.0'
@@ -46,7 +35,6 @@ def signup():
 def count_me(input_str):
     return input_str
 
-#Get genres
 @app.route('/genres')
 def genres():
 	return jsonify({ 'names': { 'first_name': 'Frank', 'last_name': 'Sinatra'}, 'score': 98})
