47b19c0a4c756313d10d806694682669d8d2f84a
im905457 <c6175@hotmail.com>
2016-09-14 15:53:35 +0800

    SQLAlchemy testing

--- flaskapp_test2.py
+++ flaskapp_test2.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.sql import select
 app = Flask(__name__)
 engine = create_engine("mysql+pymysql://im905457:19860601@aws-rds.cm1lnnlrbky4.ap-northeast-1.rds.amazonaws.com/innodb?charset=utf8",encoding="utf-8", echo=True)
-
+conn = engine.connect()
 @app.route('/')
 def hello_world():
 	s = select([MEMBER_DATA])
