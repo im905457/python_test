a198e6961a770e3af3b774d39abf24f34c56e776
im905457 <c6175@hotmail.com>
2016-09-14 17:11:37 +0800

    SQLAlchemy testing

--- flaskapp_test2.py
+++ flaskapp_test2.py
@@ -3,13 +3,14 @@ from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData, 
 from sqlalchemy.sql import text
 
 app = Flask(__name__)
+engine = create_engine("mysql+pymysql://im905457:19860601@aws-rds.cm1lnnlrbky4.ap-northeast-1.rds.amazonaws.com:3306/innodb?charset=utf8",encoding="utf-8", echo=True)
+#engine_is = create_engine("mysql+pymysql://im905457:19860601@aws-rds.cm1lnnlrbky4.ap-northeast-1.rds.amazonaws.com:3306/information_schema?charset=utf8",encoding="utf-8", echo=True)
 
 @app.route('/')
 def hello_world():
-	engine = create_engine("mysql+pymysql://im905457:19860601@aws-rds.cm1lnnlrbky4.ap-northeast-1.rds.amazonaws.com:3306/innodb?charset=utf8",encoding="utf-8", echo=True)
 	conn = engine.connect()
 	s = text("SELECT * FROM MEMBER_DATA WHERE MEMBER_SN = :id")
-	result = conn.execute(s, id='0000000001')
+	result = conn.connect(s, id='0000000001')
 	row = result.fetchone()
 	#print("name:", row['MEMBER_SN'], "; fullname:", row['MEMBER_ID'])
 	return row['MEMBER_ID']
