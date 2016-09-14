b3c9caddb0a4b3439b41931cd33389ddfdcd5e1f
im905457 <c6175@hotmail.com>
2016-09-14 15:48:38 +0800

    SQLAlchemy testing

--- flaskapp_test2.py
+++ flaskapp_test2.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from sqlalchemy import create_engine
 from sqlalchemy.sql import select
+app = Flask(__name__)
 engine = create_engine("mysql+pymysql://im905457:19860601@aws-rds.cm1lnnlrbky4.ap-northeast-1.rds.amazonaws.com/innodb?charset=utf8",encoding="utf-8", echo=True)
 
 @app.route('/')
