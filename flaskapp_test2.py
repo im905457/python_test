3b71406096bba9260717531416ab463e83df8a99
im905457 <c6175@hotmail.com>
2016-09-14 15:40:23 +0800

    SQLAlchemy testing

--- flaskapp_test2.py
+++ flaskapp_test2.py
@@ -7,8 +7,9 @@ engine = create_engine("mysql://im905457:19860601@aws-rds.cm1lnnlrbky4.ap-northe
 def hello_world():
 s = select([MEMBER_DATA])
 result = conn.execute(s)
-for row in result:
-	print(row)
+row = result.fetchone()
+	print("name:", row['MEMBER_SN'], "; fullname:", row['MEMBER_ID'])
+
 result.close()
 
 if __name__ == '__main__':
