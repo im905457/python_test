a124b4e9cb5542083573dd06db0d9162148c70dd
im905457 <c6175@hotmail.com>
2016-09-14 16:17:01 +0800

    SQLAlchemy testing

--- flaskapp_test2.py
+++ flaskapp_test2.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-	engine = create_engine("mysql+pymysql://im905457:19860601@aws-rds.cm1lnnlrbky4.ap-northeast-1.rds.amazonaws.com/innodb?charset=utf8",encoding="utf-8", echo=True)
+	engine = create_engine("mysql+pymysql://im905457:19860601@aws-rds.cm1lnnlrbky4.ap-northeast-1.rds.amazonaws.com:3306/innodb?charset=utf8",encoding="utf-8", echo=True)
 	conn = engine.connect()
-	s = select([MEMBER_DATA])
+	s = select(['MEMBER_DATA'])
 	result = conn.execute(s)
 	row = result.fetchone()
 	print("name:", row['MEMBER_SN'], "; fullname:", row['MEMBER_ID'])
